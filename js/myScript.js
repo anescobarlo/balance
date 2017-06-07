a26d6ddeb564cf7aba3522a144659aadd309eefc
Andrea Escobar <anescobarlo@gmail.com>
2017-06-07 16:21:31 +1000

    first version one card flips

---
+++ js/myScript.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+     $("#card").flip({
+         axis: 'y',
+         trigger: 'hover'
+     });
+ });
+
+
+// $(function(){
+//     $(".flip").flip({
+//         trigger: 'hover'
+//     });
+// });
+
+
